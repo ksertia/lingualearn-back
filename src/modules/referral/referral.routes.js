eed8e2ad7e737d7bbe29920b407fe70545059f89
bako110 <bakorobert2000@gmail.com>
2026-07-10 09:49:24 +0000

    fix: corriger chemin middleware auth dans referral.routes (authMiddleware)

--- src/modules/referral/referral.routes.js
+++ src/modules/referral/referral.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('./referral.controller');
-const { authenticate } = require('../../middleware/auth');
+const { authenticate } = require('../../middleware/authMiddleware');
 
 /**
  * @swagger
